76338b3c349e11005830ef8f2e666d21924ae203
GallantJoseph <gallant.joseph@outlook.com>
2025-04-13 19:55:46 -0230

    Linear Clock improvements
    Clock layers
    -Box
    -Digits
    -Hand

--- josephgallantportfolio/src/components/Clock.jsx
+++ josephgallantportfolio/src/components/Clock.jsx
@@ -1,7 +1,42 @@
+import "./Clock.css";
+
 const Clock = () => {
   return (
-    <section id="linear-clock">
-      <h1>Linear Clock</h1>
+    <section id="clock-section">
+      <h2>Linear Clock</h2>
+      <div id="linear-clock">
+        <div id="clock-numbers">
+          <ul>
+            <li className="large-digit">0</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="small-digit">3</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="large-digit">6</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="small-digit">9</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="large-digit">12</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="small-digit">15</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="large-digit">18</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="small-digit">21</li>
+            <li>|</li>
+            <li>|</li>
+            <li className="large-digit">24</li>
+          </ul>
+        </div>
+        <div id="clock-range"></div>
+        <div id="clock-hand"></div>
+      </div>
     </section>
   );
 };
