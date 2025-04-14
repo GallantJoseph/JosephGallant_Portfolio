94140e00cafaef70616ea09e823557f02c09b615
GallantJoseph <gallant.joseph@outlook.com>
2025-04-13 23:38:51 -0230

    Clock React
    More code for clock functionality in React

--- josephgallantportfolio/src/components/Clock.jsx
+++ josephgallantportfolio/src/components/Clock.jsx
@@ -1,6 +1,90 @@
 import "./Clock.css";
+import { useEffect, useState } from "react";
 
 const Clock = () => {
+  const CLOCK_PADDING = 64;
+  const DIGITS_WIDTH = 40;
+
+  // Range used to populate the options in select component
+  const MIN_HOURS = 0;
+  const MAX_HOURS = 24;
+
+  // Default values for the minimum and maximum range
+  const DEFAULT_MIN_RANGE = 9;
+  const DEFAULT_MAX_RANGE = 17;
+
+  const [minRangeValue, setMinRangeValue] = useState(DEFAULT_MIN_RANGE);
+  const [maxRangeValue, setMaxRangeValue] = useState(DEFAULT_MAX_RANGE);
+
+  // Default values for 9 and 15 range
+  const [rangeXPos, setRangeXPos] = useState(444);
+  const [rangeWidth, setRangeWidth] = useState(240);
+
+  const [options, setOptions] = useState([]);
+
+  // Seconds useState variable
+  const now = new Date();
+
+  const [hours, setHours] = useState(
+    now.getHours() + now.getHours() + now.getMinutes() / 60
+  );
+  const [minutes, setMinutes] = useState(now.getMinutes());
+  const [seconds, setSeconds] = useState(now.getSeconds());
+
+  useEffect(() => {
+    // Populate the options array that will be used for the select components
+    let opts = [];
+
+    for (let i = MIN_HOURS; i <= MAX_HOURS; i++) {
+      opts.push(i.toString());
+    }
+
+    setOptions(opts);
+
+    // Update the time
+    setInterval(() => {
+      const currTime = new Date();
+
+      setSeconds(currTime.getSeconds());
+      setMinutes(currTime.getMinutes());
+      setHours(currTime.getHours() + currTime.getMinutes() / 60);
+    }, 1000);
+  }, []);
+
+  // Update when the minRange or maxRange change
+  useEffect(() => {
+    resizeRange();
+  }, [minRangeValue, maxRangeValue]);
+
+  const handleMinRangeChange = (value) => {
+    if (parseInt(value) < maxRangeValue) {
+      setMinRangeValue(value);
+    } else {
+      alert(
+        "The value must be less than the max value. Please select a different value."
+      );
+    }
+  };
+
+  const handleMaxRangeChange = (value) => {
+    if (parseInt(value) > minRangeValue) {
+      setMaxRangeValue(value);
+    } else {
+      alert(
+        "The value must be greater than the min value. Please select a different value."
+      );
+    }
+  };
+
+  const resizeRange = () => {
+    let diff = Math.abs(maxRangeValue - minRangeValue);
+
+    setRangeWidth(diff * DIGITS_WIDTH);
+    setRangeXPos(
+      CLOCK_PADDING + DIGITS_WIDTH / 2 + DIGITS_WIDTH * minRangeValue
+    );
+  };
+
   return (
     <section id="clock-section">
       <h2>Linear Clock</h2>
@@ -34,8 +118,46 @@ const Clock = () => {
             <li className="large-digit">24</li>
           </ul>
         </div>
-        <div id="clock-range"></div>
-        <div id="clock-hand"></div>
+        <div
+          id="clock-range"
+          style={{ width: rangeWidth, left: rangeXPos }}
+        ></div>
+        <div id="clock-hand" style={{ left: hours * DIGITS_WIDTH }}></div>
+        <div id="clock-seconds">
+          <span id="seconds">{seconds}</span>
+        </div>
+      </div>
+      <div id="clock-min-max">
+        <label htmlFor="minRangeSelect">Min range</label>
+        <select
+          name="minRangeSelect"
+          id="minRangeSelect"
+          onChange={(e) => {
+            handleMinRangeChange(e.target.value);
+          }}
+          value={minRangeValue}
+        >
+          {options.map((opt) => (
+            <option key={opt} value={opt}>
+              {opt}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="maxRangeSelect">Max range</label>
+        <select
+          name="maxRangeSelect"
+          id="maxRangeSelect"
+          onChange={(e) => {
+            handleMaxRangeChange(e.target.value);
+          }}
+          value={maxRangeValue}
+        >
+          {options.map((opt) => (
+            <option key={opt} value={opt}>
+              {opt}
+            </option>
+          ))}
+        </select>
       </div>
     </section>
   );
